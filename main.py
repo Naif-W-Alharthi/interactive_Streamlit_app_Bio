6614b8724feff79fd21a3fb99cb5dd64d1f50a46
Nan <119677553+Naif-W-Alharthi@users.noreply.github.com>
2024-05-06 12:31:30 +0300

    final

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ import plotly.express as px
 import pandas as pd
 import altair as alt
 import plotly.graph_objects as go
+import numpy as np
 # Load Excel file
 
-def make_gauge(percent, input_text, input_color):
+def make_gauge(percent, input_text, input_color, width=200, height=150):
     fig = go.Figure(go.Indicator(
         mode = "gauge+number",
         value = percent,
@@ -13,8 +14,11 @@ def make_gauge(percent, input_text, input_color):
         gauge = {
             'axis': {'range': [None, 100]},
             'bar': {'color': input_color},
-            
-            }))
+        }
+    ))
+
+    # Set the width and height of the figure
+    fig.update_layout(width=width, height=height)
 
     return fig
 st.set_page_config(
@@ -23,11 +27,19 @@ st.set_page_config(
     layout="wide",
     initial_sidebar_state="expanded"
 )
-col1, col2, col3= st.columns((1,1,3))
+col1, col3= st.columns((1,1))
 alt.themes.enable("dark")
 excel_file_path = "4Lenses copy.xlsx"
 df = pd.read_excel(excel_file_path)
 
+excel_file_path = "2_Clean wheat-yields.csv"
+df_wheat = pd.read_csv(excel_file_path)
+excel_file_path = "2_Clean Tree Cover Loss.csv"
+df_tree = pd.read_csv(excel_file_path)
+excel_file_path = "2_Clean global-living-planet-index.csv"
+df_living = pd.read_csv(excel_file_path)
+excel_file_path = "2_Clean deforestation-co2-trade-by-product.csv"
+df_defor = pd.read_csv(excel_file_path)
 
 st.markdown(
     """
@@ -43,6 +55,8 @@ st.markdown(
 [data-testid="stMarkdownContainer"] {
     font-size: 500px !important;
 }
+
+ .stMetric { text-align: center; display: flex; align-items: center; justify-content: center; }
 </style>
 """,
     unsafe_allow_html=True,
@@ -50,11 +64,11 @@ st.markdown(
 # st.write(df)
 
 
-st.title("Main Page")
-st.write("""
-My first app
-Hello *world!*
-""")
+# st.title("Main Page")
+# st.write("""
+# My first app
+# Hello *world!*
+# """)
 
 
 
@@ -63,44 +77,68 @@ with st.sidebar:
     st.title('🌎 Biodiversity Dashboard')
     year_list = list(df.year.unique())[::-1]
     year_list=year_list[1::]
-    print(year_list,"year list")
+   
     selected_year = st.selectbox('Select a year', year_list)
-    
     df_selected_year = df[df.year == selected_year ]
+    
+    df_living_list= list(df_living.Country.unique())
+    
+   
+    selected_country = st.selectbox('Select a country', df_living_list)
+    df_selected_country = df_living[df_living.Country == selected_country ]
+    df_defor_list = list(df_defor.Country.unique())
+    
+    selected_country_defor = st.selectbox('Select a country for radar', df_defor_list)
+
+    df_defor_selected = df_defor[df_defor.Country == selected_country_defor]
     
 # add all later
 forest_area_percent = float(df_selected_year['Forest area %'].iloc[0])
 bio_diversity_score = float(df_selected_year['Biodiversity score'].iloc[0])
-forest_area_percent = float(df_selected_year['Forest area %'].iloc[0])
-print(forest_area_percent)
+Agri = float(df_selected_year['Agricultral land %'].iloc[0])
+wheat_yeild = float(df_selected_year['wheat yeild'].iloc[0])
+
 
 input_text = "Forest Area"
 input_color = "green"
+gauge_width = 400
+gauge_height = 400
 
-gauge_chart = make_gauge(forest_area_percent, input_text, input_color)
-gauge_chart2 = make_gauge(bio_diversity_score, "Bio score", input_color)
-# with col1:
-#     st.plotly_chart(gauge_chart, use_container_width=True)
+gauge_chart = make_gauge(forest_area_percent, input_text, "orange", width=gauge_width, height=gauge_height)
+gauge_chart2 = make_gauge(bio_diversity_score, "Bio score", input_color, width=gauge_width, height=gauge_height)
+gauge_chart3 = make_gauge(Agri, "Agriculture", "blue", width=gauge_width, height=gauge_height)
+gauge_chart4 = make_gauge(wheat_yeild, "Wheat yield", "yellow", width=gauge_width, height=gauge_height)
 
+with col1:
+   line_chart = px.line(df_selected_country, x='Year', y='Living Planet Index', title='line Chart of Living plannet index for '+selected_country)
+   st.plotly_chart(line_chart)
+   st.write(gauge_chart)
+   st.write(gauge_chart2)
 # with col2:
-#     st.plotly_chart(gauge_chart, use_container_width=True)
+    
 
 with col3:
     if selected_year != 2001.0:
     
  
         lastyear_percent = float(df[df.year == selected_year-1]['Forest area %'].iloc[0])
+        
        
 
-    st.metric(label="Biodiversity score", value=round(bio_diversity_score), delta=round(bio_diversity_score)-round(lastyear_percent))
-
-if selected_year != 2020.0:
-    # df_after = df[df.year == selected_year+1 ]
+    st.metric(label="Global Biodiversity score", value=round(bio_diversity_score), delta=round(bio_diversity_score)-round(lastyear_percent))
     
-    next_year_percent = float(df[df.year == selected_year+1]['Forest area %'].iloc[0])
-    st.subheader(f"Forest area percentage ({selected_year+1}): {next_year_percent}")
 
+    df_pivot = df_defor_selected.pivot_table(index='Country', columns='Products', values='CO2 (in Tonnes)').reset_index()
+  
+    df_melted = pd.melt(df_pivot, id_vars=["Country"], var_name="Products", value_name="CO2 (in Tonnes)")
+    df_melted['Sqrt_CO2'] = df_melted['CO2 (in Tonnes)'] ** 0.5
+    # Radar chart
+    fig = px.line_polar(df_melted, r='Sqrt_CO2', theta='Products', line_close=True)
 
+    # Render radar chart
+    st.plotly_chart(fig)
+    st.write(gauge_chart3)
+    st.write(gauge_chart4)
 
 # Tree cover for country over time? 8/10
 # add radar chart for the given country and the loses
@@ -108,10 +146,10 @@ if selected_year != 2020.0:
 # Displaying forest area percentage for previous year
 
 
-line_chart = px.line(df, x='year', y='Forest area %', title='line Chart of Wheat Yield by Year')
+# line_chart = px.line(df, x='year', y='Forest area %', title='line Chart of Wheat Yield by Year')
 
-# st.write(line_chart)
-
-
-st.plotly_chart(line_chart)
+# st.write(gauge_chart)
+# st.write(gauge_chart2)
+# st.write(gauge_chart3)
+# st.write(gauge_chart4)
 # with interactive:
