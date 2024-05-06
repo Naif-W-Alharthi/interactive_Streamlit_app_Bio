a16e302d164cd5e6a0ede3dfd1d1371c060714d8
Nan <119677553+Naif-W-Alharthi@users.noreply.github.com>
2024-05-06 15:08:23 +0300

    removed comments

--- main.py
+++ main.py
@@ -61,14 +61,6 @@ st.markdown(
 """,
     unsafe_allow_html=True,
 )
-# st.write(df)
-
-
-# st.title("Main Page")
-# st.write("""
-# My first app
-# Hello *world!*
-# """)
 
 
 
@@ -77,22 +69,16 @@ with st.sidebar:
     st.title('🌎 Biodiversity Dashboard')
     year_list = list(df.year.unique())[::-1]
     year_list=year_list[1::]
-   
     selected_year = st.selectbox('Select a year', year_list)
     df_selected_year = df[df.year == selected_year ]
-    
     df_living_list= list(df_living.Country.unique())
-    
-   
     selected_country = st.selectbox('Select a country', df_living_list)
     df_selected_country = df_living[df_living.Country == selected_country ]
     df_defor_list = list(df_defor.Country.unique())
-    
     selected_country_defor = st.selectbox('Select a country for radar', df_defor_list)
-
     df_defor_selected = df_defor[df_defor.Country == selected_country_defor]
     
-# add all later
+
 forest_area_percent = float(df_selected_year['Forest area %'].iloc[0])
 bio_diversity_score = float(df_selected_year['Biodiversity score'].iloc[0])
 Agri = float(df_selected_year['Agricultral land %'].iloc[0])
@@ -101,8 +87,8 @@ wheat_yeild = float(df_selected_year['wheat yeild'].iloc[0])
 
 input_text = "Forest Area"
 input_color = "green"
-gauge_width = 400
-gauge_height = 400
+gauge_width = 300
+gauge_height = 300
 
 gauge_chart = make_gauge(forest_area_percent, input_text, "orange", width=gauge_width, height=gauge_height)
 gauge_chart2 = make_gauge(bio_diversity_score, "Bio score", input_color, width=gauge_width, height=gauge_height)
@@ -114,42 +100,18 @@ with col1:
    st.plotly_chart(line_chart)
    st.write(gauge_chart)
    st.write(gauge_chart2)
-# with col2:
+
     
 
 with col3:
     if selected_year != 2001.0:
-    
- 
         lastyear_percent = float(df[df.year == selected_year-1]['Forest area %'].iloc[0])
-        
-       
-
     st.metric(label="Global Biodiversity score", value=round(bio_diversity_score), delta=round(bio_diversity_score)-round(lastyear_percent))
-    
-
     df_pivot = df_defor_selected.pivot_table(index='Country', columns='Products', values='CO2 (in Tonnes)').reset_index()
-  
     df_melted = pd.melt(df_pivot, id_vars=["Country"], var_name="Products", value_name="CO2 (in Tonnes)")
     df_melted['Sqrt_CO2'] = df_melted['CO2 (in Tonnes)'] ** 0.5
-    # Radar chart
     fig = px.line_polar(df_melted, r='Sqrt_CO2', theta='Products', line_close=True)
-
-    # Render radar chart
     st.plotly_chart(fig)
     st.write(gauge_chart3)
     st.write(gauge_chart4)
 
-# Tree cover for country over time? 8/10
-# add radar chart for the given country and the loses
-
-# Displaying forest area percentage for previous year
-
-
-# line_chart = px.line(df, x='year', y='Forest area %', title='line Chart of Wheat Yield by Year')
-
-# st.write(gauge_chart)
-# st.write(gauge_chart2)
-# st.write(gauge_chart3)
-# st.write(gauge_chart4)
-# with interactive:
